3b22e03d7366d5e1be8cdf63385afe5944fc2b46
Jo Darren Adriel <darren12@rocketmail.com>
2023-07-31 09:51:10 +0700

    Added URL for Sepoila

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.19",
   networks: {
     sepoila: {
-      url: "",
+      url: "https://eth-sepolia.g.alchemy.com/v2/iE-q0rNGm-ZNE4wFJ69pwgQPhP8wASnk",
       accounts: ["78712e759a9f794161cd7a79685129f7ac03c46302d3fc46a51d54a47f73c5bd"]
 
     }
