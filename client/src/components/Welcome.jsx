c79982c936f8da19155654789c80274e4412f3f7
Jo Darren Adriel <darren12@rocketmail.com>
2023-08-01 11:11:51 +0700

    Added form function

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -20,10 +20,16 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
   );
 
 const Welcome = () => {
-    const { connectWallet, currentAccount } = useContext(TransactionContext);
+    const { connectWallet, currentAccount, formData, sendTransaction, handleChange } = useContext(TransactionContext);
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message } = formData;
 
+        e.preventDefault();
+
+        if(!addressTo || !amount || !keyword || !message) return alert("Please fill all the fields");
+
+        sendTransaction();
     }
 
     return(
@@ -88,16 +94,16 @@ const Welcome = () => {
                         </div>
                     </div>
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}} />
-                        <Input placeholder="Amount" name="amount" type="number" handleChange={() => {}} />
-                        <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={() => {}} />
-                        <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}} />
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={ handleChange } />
+                        <Input placeholder="Amount" name="amount" type="number" handleChange={ handleChange } />
+                        <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={ handleChange } />
+                        <Input placeholder="Enter Message" name="message" type="text" handleChange={ handleChange } />
 
                         <div className="h-[1px] w-full bg-gray-100 my-2" />
 
                         {false ? (<Loader />) : 
                             (<button type="button" 
-                                onClick={handleSubmit} 
+                                onClick={ handleSubmit } 
                                 className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer">
                             Send Now
                             </button>
